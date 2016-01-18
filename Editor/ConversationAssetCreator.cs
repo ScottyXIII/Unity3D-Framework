f94c193d0960508657fa09f53811b627c199d8b9
steakyfask <scotty6663@hotmail.com>
2016-01-18 03:10:12 +0000

    editor utility

---
+++ Editor/ConversationAssetCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+ 
+public class ConversationAssetCreator : MonoBehaviour {
+	
+	[MenuItem("Assets/Create/Conversation")]
+	public static void CreateConversationAsset()
+	{
+		CustomAssetUtility.CreateAsset<Conversation> (); 
+	}
+
+ 
+}
